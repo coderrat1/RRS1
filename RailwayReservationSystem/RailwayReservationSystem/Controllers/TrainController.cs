17d9249083194cf131b7ff86066ea561fc1f42bb
coderrat1 <vermashubhamfy@gmail.com>
2022-10-03 15:29:17 +0530

    committing RRS1

---
+++ RailwayReservationSystem/RailwayReservationSystem/Controllers/TrainController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RailwayReservationSystem.Data;
+using RailwayReservationSystem.Data.Repository;
+using RailwayReservationSystem.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RailwayReservationSystem.Controllers
+{
+    [Route("Train")]
+    public class TrainController : ControllerBase
+    {
+        private readonly ITrainRepository _repo;
+        private readonly EmailRepository _email;
+
+        public TrainController(ITrainRepository repo, EmailRepository email)
+        {
+            _repo = repo;
+            _email = email;
+        }
+
+        #region "SearchTrain"
+        [HttpPost]
+        [Route("Search")]
+        public IActionResult SearchTrain([FromBody] SearchTrain search)
+        {
+
+            if (ModelState.IsValid)
+            {
+                List<Train> trains = _repo.SearchTrainDetails(search);
+                if (trains != null)
+                {
+                    return Ok(trains);
+                }
+                return NotFound(new { msg = "No train for this Search..." });
+            }
+
+            return ValidationProblem("Fill the data Properly...");
+        }
+        #endregion
+
+        #region "UpdateTrain"
+        [Authorize(Roles = "Admin")]
+        [HttpPut]
+        [Route(("Update"))]
+        public IActionResult UpdateTrain([FromBody] TrainDtoput trainDtoput)
+        {
+            if (ModelState.IsValid)
+            {
+                Train train = _repo.UpdateTrainDetails(trainDtoput);
+
+                if (train == null)
+                {
+                    return NotFound(new { msg = "No Train Found...." });
+                }
+
+                return Ok(train);
+            }
+
+            return ValidationProblem("Fill the data Properly...");
+        }
+        #endregion
+
+        #region "AddTrain"
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("Add")]
+        public ActionResult<Train> AddTrain([FromBody] TrainDto trainDto)
+        {
+            if (ModelState.IsValid)
+            {
+                Train train = _repo.AddTrainDetails(trainDto);
+                if (train == null)
+                    return Conflict(new { msg = "Some error happens...Try Again" });
+
+                return CreatedAtAction("AddTrain", train);
+            }
+
+            return ValidationProblem("Fill the data Properly...");
+        }
+        #endregion
+
+
+        //[HttpGet]
+        //[Route("ViewTrain")]
+        //public async Task<ActionResult<IEnumerable<Train>>> GetTrains()
+        //{
+        //    return Ok(await _context.Trains.ToListAsync());
+        //}
+
+
+        //[HttpGet("ViewTrain/{id}")]
+        //public async Task<ActionResult<Train>> GetTrain(int id)
+        //{
+        //    var train = await _context.Trains.FindAsync(id);
+
+        //    if (train == null)
+        //    {
+        //        return NotFound(new { msg = "Train Not available for this Search" });
+        //    }
+
+        //    return Ok(train);
+        //}
+    }
+}
---
+++ RailwayReservationSystem/RailwayReservationSystem/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using RailwayReservationSystem.Data;
+using RailwayReservationSystem.Data.Repository;
+using RailwayReservationSystem.Models;
+
+using System.Threading.Tasks;
+
+namespace RailwayReservationSystem.Controllers
+{
+    [Route("User")]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IUserRepository _repo;
+        private readonly EmailRepository _mail;
+
+        public UserController(IConfiguration configuration, IUserRepository repo, EmailRepository mail)
+        {
+            _configuration = configuration;
+            _repo = repo;
+            _mail = mail;
+        }
+
+        #region "Login Functionality"
+        [HttpPost]
+        [Route("Login")]
+        public ActionResult Login([FromBody] Login login)
+        {
+            User user = _repo.CheckUser(login);
+            if(user == null)
+                return NotFound(new { msg = "User Not Found..."});
+
+            var token = _repo.GenerateJWT(user);
+            return Ok(token);
+
+        }
+
+        
+        #endregion
+
+        #region "Register Functionality"
+        [HttpPost]
+        [Route("Register")]
+        public async Task<ActionResult> Register([FromBody]Register reg)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_repo.CheckEmail(reg))
+                {
+                    User user = _repo.AddUser(reg);
+
+                    MailDetails mailDetails = new MailDetails
+                    {
+                        To = user.Email,
+                        Subject = "Account Successfully Created"
+                    };
+
+                    await _mail.SendEmail(mail: mailDetails, userId: user.UserId);
+
+                    return CreatedAtAction("Register", user);
+                }
+                else
+                {
+                    return Conflict(new { msg = "Email Already Exists..." });
+                }
+            }
+            else
+            {
+                return ValidationProblem("Check all fields");
+            }
+        }
+        #endregion
+    }
+}
